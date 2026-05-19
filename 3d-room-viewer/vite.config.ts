894f38c49c935f9466547e9d0008808f29a80679
Sama Abdulazizova <sama@Nurlanas-MacBook-Air.local>
2026-05-19 10:10:33 +0400

    configured

--- 3d-room-viewer/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
-import vercel from 'vite-plugin-vercel/vite'
-
-
-// https://vite.dev/config/
-export default defineConfig({
-    plugins: [react(), vercel()],
-})
